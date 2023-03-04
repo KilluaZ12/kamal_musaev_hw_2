221b69b1fa663e5debfb10a07445ae87d31d541e
KilluaZ12 <kamalmusaev.12.92@gmail.com>
2023-03-04 18:43:20 +0600

    метод который определяет: можно идти гулять или нет

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String answer = getAnswer(generateRandomAge(), 29);
+        System.out.println(answer);
+        String answer2 = getAnswer(generateRandomAge(), 25);
+        System.out.println(answer2);
+        String answer3 = getAnswer(generateRandomAge(), 32);
+        System.out.println(answer3);
+        String answer4 = getAnswer(generateRandomAge(), 28);
+        System.out.println(answer4);
+        String answer5 = getAnswer(generateRandomAge(), 15);
+        System.out.println(answer5);
+
     }
+
+    public static String getAnswer(int age, int temperature){
+        String answer;
+        if(age >= 20 && age <= 45 && temperature >= -20 && temperature < 30){
+            answer = "Можно идти гулять";
+        }else if (age < 20 && temperature > 0 && temperature < 28){
+            answer = "Можно идти гулять";
+        }else if (age > 45 && temperature > -10 && temperature < 25){
+            answer = "Можно идти гулять";
+        }else {
+            answer = "Оставайтесь дома";
+        }
+        return answer;
+    }
+
+    public static int generateRandomAge(){
+        Random newRandom = new Random();
+        return newRandom.nextInt(0 , 100);
+    }
+
 }
